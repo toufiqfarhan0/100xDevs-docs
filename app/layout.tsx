8940e3d2a0b1ab7b5a41ef3cbb59668c88ec4a82
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2024-08-26 11:20:45 +0530

    Authenticated User with Real-Time Features

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -41,7 +42,9 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
+          <Provider>
           {children}
+          </Provider>
         </body>
       </html>
     </ClerkProvider>
