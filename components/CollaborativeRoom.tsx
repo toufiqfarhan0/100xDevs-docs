d88f91def2171ac1febb17694da097148e1b389f
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2024-08-30 12:19:24 +0530

    share modal where user can share document

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,6 +10,7 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 import Loader from "./Loader";
+import ShareModal from "./ShareModal";
 
 const CollaborativeRoom = ({
   roomId,
@@ -113,6 +114,14 @@ const CollaborativeRoom = ({
             </div>
             <div className="flex w-full flex-1 justify-end gap-2">
               <ActiveCollaborators />
+
+              <ShareModal 
+              roomId={roomId}
+              collaborators={users}
+              creatorId={roomMetadata.creatorId}
+              currentUserType={currentUserType}
+              />
+
               <SignedOut>
                 <SignInButton />
               </SignedOut>
